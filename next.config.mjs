2b89534edc31551eab4605c280dc126eaaab36be
Daniel <elpepe2079@protonmail.com>
2024-02-11 21:57:40 -0500

    not-finished

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+module.exports = {
     eslint: {
-        ignoreDuringBuilds: true,
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
     },
-/* ...Your other config rules */
-}
-
-module.exports = nextConfig
+  }
