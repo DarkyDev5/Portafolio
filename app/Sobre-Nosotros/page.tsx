a7992b30007eaff44254240f9a369421b59d05cf
DarkyDev5 <daniel.ca.pe207@gmail.com>
2024-06-25 02:13:19 -0500

    Main-About

--- app/Sobre-Nosotros/page.tsx
+++ app/Sobre-Nosotros/page.tsx
@@ -1,28 +1,13 @@
 "use client";
-import dynamic from 'next/dynamic';
-import { useEffect, useState } from 'react';
 import Navbar from '../MainPage/navbar';
 import TextContent from './TextContent';
-import LeadershipSection from '../components/LeadershipSection';
+
 import Parallax from '../components/Parallax';
-
-// Dynamic imports for the components
-
-
-const AstroCanvas = dynamic(() => import('../components/AboutMain').then(mod => mod.AstroCanvas), {
-    ssr: false,
-    loading: () => <p>Loading AstroCanvas...</p>,
-});
 
 
 
 export default function About() {
-    const [loadCanvases, setLoadCanvases] = useState(false);
-
-    useEffect(() => {
-        const timer = setTimeout(() => setLoadCanvases(true), 2000);
-        return () => clearTimeout(timer);
-    }, []);
+    
 
     return (
         <div className="relative w-full h-screen">
